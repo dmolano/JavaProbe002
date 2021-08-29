d4845e0c990841f86ecb23f531e4100e7626bba0
Dionisio Molano <dmolanor@gmail.com>
2021-08-29 17:16:32 +0200

    Init

---
+++ src/es/dmr_smriti/probe/probe003/MainApp.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package es.dmr_smriti.probe.probe003;
+
+import es.dmr_smriti.probe.probe003.MainApp;
+
+/**
+ * The Class MainApp.
+ *
+ * @author dmr-smriti
+ */
+public class MainApp {
+
+	/**
+	 * Instantiates a new main app.
+	 */
+	public MainApp() {
+		super();
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println(MainApp.class.getName());
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dmr-smriti
+ *
+ */
+module javaProbe003 {
+}
